e5fe6c32f61c2cf4aac574714ebfe6a121370fcc
Gabriel <gabrielroberto099@gmail.com>
2022-11-09 17:01:48 -0400

    09/11

--- script/script.js
+++ script/script.js
@@ -1,13 +1,31 @@
-const wrapper = document.querySelector(".wrapper"),
+const body = document.querySelector("body"),
+//popup
+popup = body.querySelector(".popup"),
+popMessage = popup.querySelector('[message]'),
+popBtn1 = popup.querySelector("#btn-1"),
+popBtn2 = popup.querySelector("#btn-2"),
+//geral
+wrapper = body.querySelector(".wrapper"),
+musicBox = body.querySelector(".music"),
+//music-list
 musicList = wrapper.querySelector(".music-list"),
 openQueue = wrapper.querySelector("#btnqueue"),
 closeQueue = musicList.querySelector("#close");
 
-
+//music-list
 openQueue.addEventListener("click", ()=>{
-    musicList.classList.toggle("show"); 
+    musicList.classList.toggle("show");
+    musicBox.classList.toggle("show");
 });
-
 closeQueue.addEventListener("click", ()=>{
     openQueue.click();
+});
+
+//pop-up show and close
+popBtn1.addEventListener("click", ()=>{
+    body.classList.toggle("show");
+});
+
+popBtn2.addEventListener("click", ()=>{
+    body.classList.toggle("show");
 });
